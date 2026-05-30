021bb3b87a21f513c5807d2b1a8a411b9f9e6f71
Arka Prasaya <arkaprasasya@gmail.com>
2026-05-30 23:37:38 +0700

    learn about webpack

---
+++ javascript/webpack-practice/src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+import "./styles.css"
+import odinImage from "./assets/phos.jpg";
+
+const image = document.createElement("img");
+image.src = odinImage;
+
+document.body.appendChild(image);
